f2fc87acff31c74799c9eb21d238c711877f7ece
ericovinicosta <elcosta@gmail.com>
2023-03-17 19:12:36 -0300

    primeiro commit

---
+++ l5q1.py
@@ -0,0 +1,16 @@
+''' 1. Faça um programa para imprimir:
+    1
+    2   2
+    3   3   3
+    .....
+    n   n   n   n   n   n  ... n
+para um n informado pelo usuário. Use uma função que receba um valor n inteiro e imprima até a n-ésima linha.'''
+
+def repetir_vezes(numero):
+    for n in range(numero):
+        print(numero, end='\t')
+    print()
+
+numero = int(input('Digite a quantidade de vezes: '))
+for n in range(1,numero):
+    repetir_vezes(n)
